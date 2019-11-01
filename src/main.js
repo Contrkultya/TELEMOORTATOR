6c3d38414ed23b3ce2e9005493fc2e9030d63b41
Sergo Skeletonson <40607257+Contrkultya@users.noreply.github.com>
2019-11-01 20:02:11 +0500

    add firebase support
    added firebase support

--- src/main.js
+++ src/main.js
@@ -8,3 +8,14 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXQ3E5spLX7nCaGapfIRIpX2DnkLNA-MQ",
+  authDomain: "telemoortation.firebaseapp.com",
+  databaseURL: "https://telemoortation.firebaseio.com",
+  projectId: "telemoortation",
+  storageBucket: "telemoortation.appspot.com",
+  messagingSenderId: "949803695350",
+  appId: "1:949803695350:web:efabda81846db0218840d7"
+};
+firebase.initializeApp(firebaseConfig);
