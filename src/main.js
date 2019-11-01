dad2b16648274647ec95172fd28c9d52fa38cf46
Timofey <48101066+Mita57@users.noreply.github.com>
2019-11-02 00:46:54 +0500

    Added vuetify, vuex and moor <3

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
+import Vuex from 'vuex';
+import Vuetify from 'vuetify';
+
 
 Vue.config.productionTip = false;
 
