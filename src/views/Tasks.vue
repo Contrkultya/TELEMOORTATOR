9daeea403a3638eee44eb6e933dcfcd5c220949d
Timofey <48101066+Mita57@users.noreply.github.com>
2019-11-02 22:01:35 +0500

    Tasks pravoslavnoo

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,4 +1,25 @@
 <template>
+    <v-card class="mx-auto" max-width="344">
+        <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+        <v-card-title>Top western road trips</v-card-title>
+        <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+        <v-card-actions>
+            <v-btn text>Share</v-btn>
+            <v-btn color="purple" text>Explore</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+            <div v-show="show">
+                <v-divider></v-divider>
+                <v-card-text>
+                    tasks.
+                </v-card-text>
+            </div>
+        </v-expand-transition>
+    </v-card>
 </template>
 
 <script>
